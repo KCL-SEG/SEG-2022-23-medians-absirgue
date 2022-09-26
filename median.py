f367786dcae2769eb15b990452686fbc439bab33
absirgue <anton.sirgue@kcl.ac.uk>
2022-09-26 11:24:43 +0100

    Add logic relative to medians

--- median.py
+++ median.py
@@ -5,6 +5,12 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        halfLength = len(numbers)/2
+        if (halfLength % 2 == 0):
+            print(numbers[(numbers//2 + numbers//2+1)/2])
+        else:
+            print(numbers[halfLength])
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
